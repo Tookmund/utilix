e004f4cbb40a52b9041bad5e54fa9b4a6e36fa1b
Jacob Adams <tookmund@gmail.com>
2016-02-05 23:52:07 -0500

    dd: Begin dd

---
+++ dd.c
@@ -0,0 +1,22 @@
+/*
+ * dd -- Data Dump
+ *
+ * Copyright (C) 2016 Jacob Adams <tookmund@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General Public License
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+// dd [input] [output] [blocksize]
+
+int main (int argc, char* argv[]) {
+	if (argc < 3) {
+		printf("%s [input] [output] [optional-blocksize]");
+	}
+}

72f816a48d801505714c934bbd828992739d0eb2
Jacob Adams <tookmund@gmail.com>
2016-02-06 22:51:45 -0500

    *.c, Makefile, template, perm.h: License project as Expat; Comply with GPL

--- perm.h
+++ perm.h
@@ -1,5 +1,14 @@
 /* Borrow file permissions from coreutils system.h
  * http://www.gnu.org/software/coreutils/coreutils.html
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 3 of the GNU General Public License
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 /* Commonly used file permission combination.  */

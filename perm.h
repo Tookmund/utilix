628a04d8e05124b45df23d36c5dad7aaa3541886
Jacob Adams <tookmund@gmail.com>
2016-03-25 10:49:06 -0400

    perm, mkdir, new: Replace GPLed code with OpenBSD version
    Relicense all code as BSD or Expat

--- perm.h
+++ perm.h
@@ -1,15 +1,43 @@
-/* Borrow file permissions from coreutils system.h
- * http://www.gnu.org/software/coreutils/coreutils.html
+/* Permissions borrowed from OpenBSD */
+
+/*
+ * Copyright 2016 Jacob Adams <tookmund@gmail.com>
+ * Copyright (c) 1983, 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of version 3 of the GNU General Public License
- * as published by the Free Software Foundation.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
-/* Commonly used file permission combination.  */
-#define MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)
+/*
+ * The default file mode is a=rwx (0777) with selected permissions
+ * removed in accordance with the file mode creation mask.  For
+ * intermediate path name components, the mode is the default modified
+  * by u+wx so that the subdirectories can always be created.
+ */
+
+mode_t getmode()
+{
+	return ( 0777 & ~umask(0) ) | S_IWUSR | S_IXUSR;
+}

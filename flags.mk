a0fe75b7d4058570645ae74013ece26801606522
Jacob Adams <tookmund@gmail.com>
2016-08-12 22:51:54 -0400

    flags.mk Makefile utilish/Makefile: Store CFLAGS in common file

--- flags.mk
+++ flags.mk
@@ -1,4 +1,4 @@
- # Makefile for utilix
+ # Makefile CFLAGS for utilix
  # Copyright 2016 Jacob Adams <tookmund@gmail.com>
  #
  # Permission is hereby granted, free of charge, to any person obtaining a
@@ -19,69 +19,4 @@
  # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  # DEALINGS IN THE SOFTWARE.
 
-include "flags.mk"
-PREFIX ?= /usr/local/
-BIN = $(DESTDIR)/$(PREFIX)/bin
-
-MANDIR = $(DESTDIR)/$(PREFIX)/share/man/man1
-
-# Should be alphabetical
-TGTS= \
-cat \
-dd \
-env \
-ish \
-ls \
-mkdir \
-mv \
-new \
-rm \
-rmdir \
-sponge \
-stat \
-unenv \
-unpg
-
-all: ${TGTS}
-
-ish: utilish/*.c utilish/*.h
-	cd utilish && make
-clean:
-	rm -f ${TGTS}
-
-install: ${TGTS}
-	mkdir -p ${BIN}
-	for i in ${TGTS} ; \
-	do \
-		cp $${i} ${BIN} ; \
-	done
-
-uninstall: ${TGTS}
-	for i in ${TGTS} ; \
-	do \
-		rm ${BIN}/$${i} ; \
-	done
-
-# Borrowed from 9wm
-# https://github.com/nealey/9wm
-install.man:
-	mkdir -p $(MANDIR)
-	for i in ${TGTS} ; \
-	do \
-		cp $${i}.1 $(MANDIR) ; \
-	done
-
-uninstall.man:
-	for i in ${TGTS} ; \
-	do \
-		rm ${MANDIR}/$${i}.${MANSUFFIX} ; \
-	done
-
-# Generate .gitignore
-gitignore:
-	> .gitignore
-	for i in ${TGTS} ; \
-	do \
-		echo $${i} >> .gitignore ; \
-	done ; \
-	echo "*.o" >> .gitignore
+CFLAGS += -Wall -Werror -pedantic
